658212225648e066b7f1bdc552749bb3efe652cc
ilyasweb <ilyasweb0721@gmail.com>
2021-08-25 12:39:44 -0700

    added computer_guess method

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import random
 
 
 def guess(ending_num):
-
     random_num = random.randint(1, ending_num)
     guess_num = 0
 
@@ -15,4 +14,23 @@ def guess(ending_num):
     print(f"You have guessed the number {guess_num} correctly")
 
 
-guess(50)
+def computer_guess(ending_range):
+    low = 1
+    high = ending_range
+    feedback = ''
+
+    while feedback != 'c':
+        if low != high:
+            guess_c = random.randint(low, high)
+        else:
+            guess_c = low
+        feedback = input(f"is the number {guess_c} too high (h), too low (l) or correct (c)")
+
+        if feedback == 'h':
+            high = guess_c - 1
+        elif feedback == 'l':
+            low = guess_c + 1
+    print("Yay! Computer guessed my number")
+
+
+computer_guess(100)
